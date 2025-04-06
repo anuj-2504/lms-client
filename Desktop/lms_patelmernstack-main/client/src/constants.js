b181351e34e0744a1f631e147d55131151ac33fe
anuj darji <anujdarji@anujs-MacBook-Air.local>
2025-04-06 13:37:03 +0530

    Updated API base URLs for production

---
+++ Desktop/lms_patelmernstack-main/client/src/constants.js
@@ -0,0 +1,1 @@
+export const BASE_API_URL = "https://lms-server-y383.onrender.com/api/v1";
--- Desktop/lms_patelmernstack-main/client/src/features/api/authApi.js
+++ Desktop/lms_patelmernstack-main/client/src/features/api/authApi.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = "http://localhost:8080/api/v1/user/";
-
+baseURL: "https://lms-server-y383.onrender.com/api/v1/user";
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({
